ef5a4e615ad7b3b74ba9ad00dbeca65c38323db6
JustFoxx <justfoxxo@Outlook.com>
2021-11-18 20:51:03 +0100

    Added "" Added /home Added /whoami

--- src/main/kotlin/foxxo.kt
+++ src/main/kotlin/foxxo.kt
@@ -1,18 +1,39 @@
 package foxxoshell
 
 import java.util.*
+import java.util.regex.Pattern
 import kotlin.system.exitProcess
 
+object User {
+    val name = System.getProperty("user.name")
+    val homeDir = System.getProperty("user.home")
+}
+
 fun command(text: String) {
+    if(text == "") {
+        println("Type something!")
+        return
+    }
+
     val prefix = text.first()
 
     if(prefix == "/".single()) {
-        var args = text.drop(1).split(" ")
+        var arguments = ArrayList<String>()
+        var argsMatcher = Pattern.compile("([^\"]\\S*|\".+?\")\\s*").matcher(text.drop(1))
+        while (argsMatcher.find())
+            arguments.add(argsMatcher.group(1))
+        println(arguments)
+        var args = arguments.toList()
         val cmd = args[0]
         args = args.drop(1)
+
         when(cmd) {
             "test" ->
                 println(args)
+            "home" ->
+                println(User.homeDir)
+            "whoami" ->
+                println(User.name)
             "exit" ->
             {
                 println("Exited program.")
@@ -29,7 +50,7 @@ fun command(text: String) {
 fun main() {
     println("Welcome to FoxxoShell!\n")
     while (true) {
-        print("${System.getProperty("user.name")}>> ")
+        print("${User.name}>> ")
         val scanner = Scanner(System.`in`)
         val scanner_text = scanner.nextLine()
 
